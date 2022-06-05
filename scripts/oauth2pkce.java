0ba7880e268073f785bca24975e7db099656e975
tors42 <4084220+tors42@users.noreply.github.com>
2022-06-05 19:51:58 +0200

    Add scripts/oauth2pkce.java
    Add a script to demo the possibility to make use of OAuth2 PKCE on a
    public web server (i.e Lichess sends the redirect to the public web
    server after granting access to the application).

---
+++ scripts/oauth2pkce.java
@@ -0,0 +1,111 @@
+//DEPS io.github.tors42:chariot:0.0.36
+//JAVA 17+
+
+import java.net.URI;
+import java.util.stream.Collectors;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+import com.sun.net.httpserver.HttpServer;
+
+import chariot.Client;
+import chariot.api.Account.UriAndTokenExchange;
+
+class oauth2pkce {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length == 0) { System.out.println("Supply the Web Server URL, i.e http://example.com:8080/"); return; }
+        URI url = URI.create(args[0]);
+
+        System.out.println("Visit " + url + "demo.html");
+
+        String host = url.getHost();
+        int port = switch(url.getPort()) {
+            case -1 -> 80;
+            default -> url.getPort();
+        };
+        //:
+
+        URI redirectUri = URI.create(url.toString() + "redirect");
+
+
+        // Typically one would just register an URL handler in some environment where the application is deployed,
+        // but here we start the web server ourselves, so we will actually be the one binding the socket address.
+        var bindAddress = new InetSocketAddress(InetAddress.getByName(host), port);
+        var httpServer = HttpServer.create(bindAddress, 0);
+
+        httpServer.createContext("/demo.html", exchange -> {
+            String email = "";
+            String query = exchange.getRequestURI().getQuery();
+            if (query != null && query.contains("email=")) {
+                email = query.substring(query.indexOf("email=") + "email=".length());
+            }
+
+            byte[] response = """
+                <html>
+                    <head>
+                        <title>Demo</title>
+                    </head>
+                    <body>
+                        %s
+                        <form action="/fetch_email">
+                             <input type="submit" value="Fetch E-Mail address, using Lichess OAuth2 PKCE">
+                        </form>
+                    </body>
+                </html>
+                """.formatted(email)
+                .getBytes();
+
+            exchange.sendResponseHeaders(200, response.length);
+            exchange.getResponseBody().write(response);
+            exchange.close();
+        });
+
+        var client = Client.basic();
+        BlockingQueue<UriAndTokenExchange> bq = new ArrayBlockingQueue<>(1);
+
+        httpServer.createContext("/fetch_email", exchange -> {
+            var urlAndTokenExchange = client.account().oauthPKCEwithCustomRedirect(
+                    redirectUri,
+                    Client.Scope.email_read
+                    );
+            bq.add(urlAndTokenExchange);
+
+            String lichessUrl = urlAndTokenExchange.url().toURL().toString();
+            exchange.getResponseHeaders().put("Location", List.of(lichessUrl));
+            exchange.sendResponseHeaders(302, -1);
+        });
+
+        httpServer.createContext("/redirect", exchange -> {
+
+            var query = exchange.getRequestURI().getQuery();
+            var inparams = Arrays.stream(query.split("&"))
+                .collect(Collectors.toMap(
+                            s -> s.split("=")[0],
+                            s -> s.split("=")[1]
+                            ));
+            String code = inparams.get("code");
+            String state = inparams.get("state");
+
+            String emailAddress = "";
+            try {
+                var urlAndTokenExchange = bq.take();
+                var token = urlAndTokenExchange.token(code, state).get();
+                var auth = Client.auth(token);
+                emailAddress = auth.account().emailAddress().get().email();
+                auth.account().revokeToken();
+            } catch (Exception e) {}
+
+            var headers = exchange.getResponseHeaders();
+            headers.put("Location", List.of("/demo.html?email=%s".formatted(emailAddress)));
+            exchange.sendResponseHeaders(302, -1);
+        });
+
+
+        httpServer.start();
+    }
+}

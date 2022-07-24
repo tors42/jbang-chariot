4e95989bd91cddac7d39c54f7a9d908ca594f45f
tors42 <4084220+tors42@users.noreply.github.com>
2022-07-24 15:03:18 +0200

    Refactor nonthematic.java

--- scripts/nonthematic.java
+++ scripts/nonthematic.java
@@ -1,52 +1,51 @@
-//DEPS io.github.tors42:chariot:0.0.35
+//DEPS io.github.tors42:chariot:0.0.43
 //JAVA 17+
 import chariot.Client;
 import chariot.model.Enums.GameVariant;
-import java.util.concurrent.atomic.LongAdder;
 import java.nio.file.*;
+import java.util.concurrent.atomic.LongAdder;
 
 class nonthematic {
 
     public static void main(String... args) throws Exception {
-        if (args.length == 0) { System.out.println("No username provided"); System.exit(1); }
+        if (args.length == 0) { System.out.println("No username provided"); return; }
         String username = args[0];
-
-        var client = Client.basic(); // speedup: var client = Client.auth("token")
-
-        var result = client.users().byId(username);
-        if (! result.isPresent()) { System.out.println("Couldn't find %s".formatted(username)); System.exit(0); }
-        System.out.println("Fetching games of " + username);
-
-        var user = result.get();
         var path = Path.of(username + ".pgn");
         var writer = Files.newBufferedWriter(path,
                 StandardOpenOption.CREATE,
                 StandardOpenOption.TRUNCATE_EXISTING);
 
-        int total = user.count().all();
-        var counter = new LongAdder();
-        var written = new LongAdder();
+        var client = Client.basic(); // speedup: var client = Client.auth("token")
 
-        client.games().byUserId(user.id(), p -> p.pgnInJson(true)).stream()
-            .peek(__ -> {
-                counter.increment();
-                System.out.print("\r%d/%d".formatted(counter.intValue(), total));
-            })
-            .filter(game -> game.variant() != GameVariant.fromPosition)
-            .forEach(game -> {
-                try {
-                    writer.append(game.pgn());
-                    writer.flush();
-                    written.increment();
-                } catch(Exception e) {
-                    e.printStackTrace();
-                    System.exit(1);
-                }});
+        client.users().byId(username).ifPresentOrElse(user -> {
+            System.out.println("Fetching games of " + username);
 
-        if (written.intValue() > 0) {
-            System.out.format("%nWrote %d non-thematic games to %s%n", written.intValue(), path);
-        } else {
-            System.out.println("\nNo non-thematic games found!");
-        }
+            int total = user.count().all();
+            var counter = new LongAdder();
+            var written = new LongAdder();
+
+            client.games().byUserId(user.id(), p -> p.pgnInJson(true)).stream()
+                .peek(__ -> {
+                    counter.increment();
+                    System.out.format("\r%d/%d", counter.intValue(), total);
+                })
+                .filter(game -> game.variant() != GameVariant.fromPosition)
+                .forEach(game -> {
+                    try {
+                        writer.append(game.pgn());
+                        writer.flush();
+                        written.increment();
+                    } catch(Exception e) {
+                        throw new RuntimException(e);
+                    }});
+
+            if (written.intValue() > 0) {
+                System.out.format("%nWrote %d non-thematic games to %s%n",
+                        written.intValue(), path);
+            } else {
+                System.out.println("\nNo non-thematic games found!");
+            }
+        },
+        () -> System.out.println("Couldn't find " + username));
     }
 }
